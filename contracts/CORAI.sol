7c4d940d3531c45da16edd1269a419a65ff12d96
sandwizard <35904512+sandwizard@users.noreply.github.com>
2023-03-14 19:34:40 +0600

    fix
    - fix shadow variable names
    owner,_name,_symbol

--- contracts/CORAI.sol
+++ contracts/CORAI.sol
@@ -58,16 +58,16 @@ contract CORAI is ERC20, ERC20Burnable,AccessControl,ERC20Permit,Ownable{
     /// @notice Deploys the smart contract and creates mints inital sypply to "to" address
     /// @dev owner ship is transfer on deployment and deployer address has no access to any admin functions
     /// @dev pass normal erc20 parameters such as symbol name. with to address and fee address
-    constructor(uint256 _initialSupply,string memory _name,string memory _symbol,address _to,address _feeAddress) ERC20(_name, _symbol)   ERC20Permit(_name){
-        if(_to == address(0)){
+    constructor(uint256 initialSupply_,string memory name_,string memory symbol_,address to_,address feeAddress_) ERC20(name_, symbol_)   ERC20Permit(name_){
+        if(to_ == address(0)){
             revert zeroAddress();
         }
-        _transferOwnership(_to);
-        _grantRole(DEFAULT_ADMIN_ROLE, _to);
-        _grantRole(tax_exempt, _to);
-        _grantRole(tax_exempt, feeAddress);
-        feeAddress= _feeAddress;
-        _mint(_to,_initialSupply);
+        _transferOwnership(to_);
+        _grantRole(DEFAULT_ADMIN_ROLE, to_);
+        _grantRole(tax_exempt, to_);
+        _grantRole(tax_exempt, feeAddress_);
+        feeAddress= feeAddress_;
+        _mint(to_,initialSupply_);
         maxTxAmount = 1000000000000000000000;
         
     }
@@ -116,9 +116,9 @@ contract CORAI is ERC20, ERC20Burnable,AccessControl,ERC20Permit,Ownable{
     /// @dev else tax and limit(only sale) is applied when a lp pool is involved
     /// @dev in case on both sender and receiver is lp pool no tax or limit applied
     function transfer(address to, uint256 amount) public virtual override returns (bool)  {
-        address owner = _msgSender();
-        _transfer(owner, to, amount);
-        _transferFees(owner,to,amount);
+        address owner_ = _msgSender();
+        _transfer(owner_, to, amount);
+        _transferFees(owner_,to,amount);
         return true;
     }
     /// @notice set the fee where txlimit in terms of token on sell to lp pools
